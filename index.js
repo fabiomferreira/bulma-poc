10384654665369e050f12dbe8994bd6d96d02478
Fabio Ferreira <fabiomf@protonmail.com>
2019-10-12 01:12:13 -0400

    Insert responsive behaviour and enhance todo list display

--- index.js
+++ index.js
@@ -2,27 +2,21 @@ function addTask() {
     var inputTask = document.getElementById('input-task');
     var taskList = document.getElementById('task-list');
     var newTask = createTaskCheckbox(inputTask.value);
-    taskList.appendChild(newTask);
+    taskList.innerHTML += newTask;
 }
 
 function createTaskCheckbox(label) {
-    var checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.addEventListener('change', function() {
-        this.parentElement.classList.toggle('task-done');
-    });
+    var checkbox = `
+        <p class="control">
+            <label class="checkbox">
+                <input type="checkbox" onchange="onTaskCheckboxChange(this)"/>
+                ${label}
+            </label>
+        </p>
+    `;
+    return checkbox;
+}
 
-    var checkboxLabel = document.createElement('span');
-    checkboxLabel.textContent = label;
-
-
-    var checkboxWrapper = document
-        .createElement('label');
-    checkboxWrapper
-        .classList
-        .add('checkbox');
-    checkboxWrapper.appendChild(checkbox);
-    checkboxWrapper.appendChild(checkboxLabel);
-
-    return checkboxWrapper;
+function onTaskCheckboxChange(element) {
+    element.parentElement.classList.toggle('task-done');
 }
