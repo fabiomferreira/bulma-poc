ea3b34e47eb3acd22e9b9354fcf22e51f781d4ae
Fabio Ferreira <fabiomf@protonmail.com>
2019-10-20 10:27:04 -0400

    Throws a success message

--- app/controllers/TaskController.js
+++ app/controllers/TaskController.js
@@ -6,12 +6,18 @@ class TaskController {
         this._tasks = new Tasks();
         this._tasksView = new TasksView('#tasks');
         this._tasksView.update(this._tasks);
+        this._message = new Message();
+        this._messageView = new MessageView('#message-container');
+        this._messageView.update(this._message);
     }
 
     add(event) {
         event.preventDefault();
+        if(!this._inputTask.value) return;
         let task = new Task(this._inputTask.value);
         this._tasks.add(task);
+        this._message.text = "Task created successfully!";
+        this._messageView.update(this._message);
         this.clearForm();
         this._tasksView.update(this._tasks);
     }
